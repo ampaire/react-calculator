9d3cc26e50378de99a1dc3c55742719c44c4b3a8
ampaire <femayapr@gmail.com>
2020-05-12 18:21:14 -0600

    finished setting up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>This is a react application</h1>,
   document.getElementById('root')
 );
 
