36cd82bd60c4942ce8e430779c7bd63e8d5a079f
ampaire <femayapr@gmail.com>
2020-05-12 20:57:35 -0600

    fix an error with the modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from "prop-types";
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
