eff3c77f409edd8c2ae9b9bd96a37442f97eb9c5
ampaire <femayapr@gmail.com>
2020-05-14 20:58:59 -0600

    Fix merge issues

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-ReactDOM.render(
-  <h1 class= 'text-center'>This is a react application</h1>,
-  document.getElementById('root'),
-);
-
