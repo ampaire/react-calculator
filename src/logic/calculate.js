fcb4b152e534e6ff2a42f8c65b0668434d2ccc09
ampaire <femayapr@gmail.com>
2020-05-20 14:14:41 -0600

    the +/- operator

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,5 +1,14 @@
-const calculate = (calculator, btn) => {
+/* eslint-disable no-unused-vars */
+const calculate = (calculator, btnName) => {
   const { total, next, operation } = calculator;
+  if (next && btnName === '+/-') {
+    return { next: next * -1 };
+  }
+
+  if (total && btnName === '+/-') {
+    return { total: total * -1 };
+  }
 };
+
 
 export default calculate;
