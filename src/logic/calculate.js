490640d1136e5237391105067185f16749fa3371
ampaire <femayapr@gmail.com>
2020-05-20 15:20:45 -0600

    Add all the components

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,33 +1,38 @@
+import operate from './operate';
+
 const calculate = (calculator, btnName) => {
   let { total, next, operation } = calculator;
-  if (next && btnName === '+/-') {
-    return { next: next * -1 };
-  }
-
-  if (total && btnName === '+/-') {
-    return { total: total * -1 };
-  }
-  if (btnName === 'AC') {
+  if (btnName === 'A/C') {
     total = null;
     next = null;
     operation = null;
-    if (btnName === '%') {
-      next = 0.01 * total;
-    }
-    return { total, next, operation };
   }
 
-  if (btnName === '.') {
-    if (next) {
-      if (next.includes('.')) return {};
-      return { total, next: `${next}.`, operation };
-    }
-    if (total) {
-      if (total.includes('.')) return {};
-      return { total: `${total}.`, next, operation };
-    }
-    return { total: '0.', next, operation };
+  if (btnName === '+/-') {
+    if (total) total *= -1;
+    if (next) next *= -1;
   }
+
+  if (btnName === '%') {
+    if (total) next = 0.01 * total;
+  }
+
+  if (btnName === '=') {
+    if (total && next) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+  }
+
+  if (['+', '-', 'x', '÷', '%'].includes(btnName)) {
+    operation = btnName;
+  } else if (operation) {
+    total += btnName;
+  } else {
+    next += btnName;
+  }
+  return { total, next, operation };
 };
 
 export default calculate;
