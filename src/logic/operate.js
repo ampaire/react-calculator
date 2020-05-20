926e6ca6da9a872c0e52e7f4eb0d1cee26fc63f8
ampaire <femayapr@gmail.com>
2020-05-20 15:12:07 -0600

    Added the default case

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,6 +3,18 @@ import Big from 'big-js';
 const operate = (numberOne, numberTwo, operation) => {
   const [one, two] = [Big(numberOne), Big(numberTwo)];
 
+  switch (operation) {
+    case '+':
+      return one.plus(two);
+    case '-':
+      return one.minus(two);
+    case 'x':
+      return one.times(two);
+    case '/':
+      return one.div(two);
+    default:
+      return '0';
+  }
 };
 
 export default operate;
