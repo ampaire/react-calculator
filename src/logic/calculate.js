d5396ef53cd177d60563cb563a87057ab4702517
ampaire <femayapr@gmail.com>
2020-05-22 10:43:05 +1200

    Add missing files

---
+++ src/logic/calculate.js
@@ -0,0 +1,42 @@
+import operate from "./operate";
+
+const calculate = (calculator, btnName) => {
+  let { total, next, operation } = calculator;
+  if (btnName === "A/C") {
+    total = null;
+    next = null;
+    operation = null;
+  }
+
+  if (btnName === "+/-") {
+    if (total) total *= -1;
+    if (next) next *= -1;
+  }
+
+  if (btnName === "%") {
+    if (total) next = 0.01 * total;
+  }
+
+  if (btnName === "=") {
+    if (total && next) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    } else if (next === "0" && operation === "/") {
+      return {
+        error: "Error: E",
+      };
+    }
+  }
+
+  if (["+", "-", "x", "/", "%"].includes(btnName)) {
+    operation = btnName;
+  } else if (operation) {
+    total += btnName;
+  } else {
+    next += btnName;
+  }
+  return { total, next, operation };
+};
+
+export default calculate;
